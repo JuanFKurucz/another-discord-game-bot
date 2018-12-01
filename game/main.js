3c2da9d26e0a797ae91e8e83ba6d8be2c15c5363
SatsuB <elektratchapda@ymail.com>
2018-12-01 22:11:50 +0100

    Revert "buying things with cookies"
    This reverts commit b12060469716c5c2479030c3a4db4b60b3d2da51.

--- game/main.js
+++ game/main.js
@@ -10,15 +10,6 @@ var users={
   //"id":UserObject
 };
 
-var buildings={
-  "1":new b.building("1","Shelter",10,5),
-  "2":new b.building("2","Shelter",50,10),
-  "3":new b.building("3","Appartment",250,100),
-  "4":new b.building("4","House",3000,1000),
-  "5":new b.building("5","Palace",50000,2000),
-};
-
-
 const prefix = "!";
 
 function commandHandler(msg){
@@ -27,20 +18,15 @@ function commandHandler(msg){
   if(text.indexOf(prefix)===0){
     text=text.substring(prefix.length,text.length);
     var command = text.split(" ");
-    var user=users[msg.author.id];
     switch(command[0]){
       case "info":
         response="You have "+users[msg.author.id].cookies+" cookies";
         break;
 
       case "buy":
-        var building=buildings[parseInt(command[1])];
-        if(building.acquire(user)){
-          response="You bought a building "+command[1]+"!";
-        }
-        else{
-          reponse="You don't have enough cookies...";
-        }
+        response="You bought a building "+command[1]+"!";
+        var building=new b.Building(parseFloat(command[1]));
+        users[msg.author.id].buildings.push(building);
         break;
 
       default:
