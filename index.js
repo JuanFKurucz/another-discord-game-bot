b913e8a2cf9cf5ca85c01a3f5252591884f000bd
Juan Francisco Kurucz <juanfkurucz@gmail.com>
2018-12-01 14:28:42 -0300

    First fix (?

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const game = require(__dirname+"/game/main.js");
-const token = '';
+const token = 'NTE4NDc3Nzc3NTcwMTAzMjk2.DuRVpw.FrIJP52YjMI_ZRr2Jr_VI0ZzhmI';
+
+const botId = "518477777570103296";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 function onMessage(msg){
-  if(msg.hasOwnProperty("author")){
+  if(msg.hasOwnProperty("author") && msg.author.id != botId){
     let text = msg.content+"";
     let response=game.read(text,msg.channel.type);
     if(response!=""){
