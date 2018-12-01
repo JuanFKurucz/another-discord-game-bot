94294095ae9fa6db4b80c81916044be73bfce6ad
Juan Francisco Kurucz <juanfkurucz@gmail.com>
2018-12-01 18:20:21 -0300

    fixes

--- game/main.js
+++ game/main.js
@@ -11,18 +11,19 @@ var users={
 };
 
 var buildings={
-  "1":new b.building("1","Shelter",10,5),
-  "2":new b.building("2","Shelter",50,10),
-  "3":new b.building("3","Appartment",250,100),
-  "4":new b.building("4","House",3000,1000),
-  "5":new b.building("5","Palace",50000,2000),
+  "1":new b.Building("1","Shelter",10,5),
+  "2":new b.Building("2","Shelter",50,10),
+  "3":new b.Building("3","Appartment",250,100),
+  "4":new b.Building("4","House",3000,1000),
+  "5":new b.Building("5","Palace",50000,2000),
 };
 
 
 const prefix = "!";
 
 function commandHandler(msg){
-@ -18,15 +27,20 @@ function commandHandler(msg){
+  let response = "";
+  let text = msg.content+"";
   if(text.indexOf(prefix)===0){
     text=text.substring(prefix.length,text.length);
     var command = text.split(" ");
@@ -33,14 +34,15 @@ function commandHandler(msg){
         break;
 
       case "buy":
-        var building=new b.Building(parseFloat(command[1]));
-        users[msg.author.id].buildings.push(building);
         var building=buildings[parseInt(command[1])];
-        if(building.acquire(user)){
-          response="You bought a building "+command[1]+"!";
-        }
-        else{
-          reponse="You don't have enough cookies...";
+        if(building){
+          if(building.acquire(user)){
+            response="You bought a building "+command[1]+"!";
+          } else{
+            response="You don't have enough cookies...";
+          }
+        } else {
+          response="This building doesn't exist";
         }
         break;
 
