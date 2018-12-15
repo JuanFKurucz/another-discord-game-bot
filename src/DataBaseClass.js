46362d7f0ff0ee68cc5a7a6ddcc7277d08317594
Juan Francisco Kurucz <juanfkurucz@gmail.com>
2018-12-15 04:40:15 -0300

    Database copy from online to local

--- src/DataBaseClass.js
+++ src/DataBaseClass.js
@@ -4,24 +4,14 @@
   This currently does nothing
 **/
 const mysql = require('mysql');
+const structure = ["building","upgrade","user","user_building","user_upgrade"];
 
 class DataBase {
 
-  constructor(enabled=true){
+  constructor(config,enabled=true){
     this.enabled = enabled;
     this.printErrors=true;
-    this.options = {
-      connectionLimit: 100,
-      /*host: "localhost",
-      user: "root",
-      password: "",
-      database: "notagame",*/
-      host: "db4free.net",
-      user: "notagame_jfk",
-      password: "alpaca123456",
-      database: "notagame",
-      debug: false
-    };
+    this.options = config;
     this.start();
   }
   setEnabled(bool){
@@ -80,19 +70,83 @@ class DataBase {
         response = response[Symbol.iterator]();
       } catch(e){
         if(this.printErrors){
-          console.error(e);
+        //  console.error(e);
         }
         response=null;
       }
     }
     return response;
   }
+
+  async copyDatabase(db){
+    if(this.connection){
+      let table,
+          underscore,
+          condition,
+          id,
+          idName,
+          tempResult;
+      for(let s=0;s<structure.length;s++){
+        table = structure[s];
+        for(let userResult of (await db.query("SELECT * FROM "+table))){
+          underscore = table.split("_");
+          condition="";
+          for(let u=0;u<underscore.length;u++){
+            if(u!=0){
+              condition+=" AND ";
+            }
+            idName="id_"+underscore[u];
+            id = userResult[idName];
+            condition += idName+"="+id;
+          }
+
+          tempResult=await this.query("SELECT * FROM "+table+" WHERE "+condition);
+          if(!tempResult.next().done){
+            this.query("UPDATE "+table+" SET ? WHERE "+condition,userResult);
+          } else {
+            this.query("INSERT INTO "+table+" SET ?",userResult);
+          }
+        }
+      }
+    }
+  }
 }
 
-const database = new DataBase();
+
+const database = new DataBase({
+  connectionLimit: 100,
+  host: "db4free.net",
+  user: "notagame_jfk",
+  password: "alpaca123456",
+  database: "notagame",
+  debug: false
+});
+
+const localDatabase = new DataBase({
+  connectionLimit: 100,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "notagame",
+  debug: false
+});
+
+
+localDatabase.copyDatabase(database);
 
 exports.dbQuery = async function(sql,object){
-  return await database.query(sql,object);
+  let result=null,
+      temp;
+  if(database.connection){
+    result = await database.query(sql,object);
+  }
+  if(localDatabase.connection){
+    temp = await localDatabase.query(sql,object);
+  }
+  if(result===null){
+    result=temp;
+  }
+  return result;
 };
 
 exports.dbChangeEnable = function(bool){
