65fc5cc3b42572665507f44eadd1922426fcf9e1
Juan Francisco Kurucz <juanfkurucz@gmail.com>
2018-12-22 23:04:22 -0300

    DataBase cleanup

--- src/DataBase.js
+++ src/DataBase.js
@@ -7,21 +7,6 @@ const mysql = require('mysql'),
       fs = require('fs');
 
 class DataBase {
-  static enabled(){}
-  static databases(){}
-  static structure(){}
-
-  static async getStructure(db1,db2){
-    DataBase.structure = [];
-    if(DataBase.enabled === true && DataBase.databases.length>1){
-      let db = DataBase.databases[0],
-          table = await db.query("SELECT TABLE_NAME FROM information_schema.tables WHERE TABLE_SCHEMA = '"+db.options.database+"'");
-      for(let t in table){
-        DataBase.structure.push(table[t]["TABLE_NAME"]);
-      }
-    }
-  }
-
   constructor(config,enabled=true){
     this.connection=null;
     this.enabled = enabled;
@@ -40,10 +25,10 @@ class DataBase {
 
   onError(err){
     console.error(err,4);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      this.setEnabled(false);                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      console.error(err);                               // server variable configures this)
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+      this.setEnabled(false);
+    } else {
+      console.error(err);
     }
   }
 
@@ -81,14 +66,6 @@ class DataBase {
     }
   }
 
-  queryPromise(sql,args) {
-    return new Promise((resolve,reject ) => {
-      this.connection.query(sql,args,(err,rows) => {
-        (err) ? reject(err) : resolve(rows);
-      });
-    });
-  }
-
   close() {
     return new Promise((resolve,reject) => {
       this.connection.end(err => {
@@ -101,7 +78,11 @@ class DataBase {
     console.log(this.options.host+"("+this.options.database+")"+": "+sql,0.5);
     let response;
     try {
-      response = await this.queryPromise(sql,object);
+      response = await new Promise((resolve,reject ) => {
+        this.connection.query(sql,args,(err,rows) => {
+          (err) ? reject(err) : resolve(rows);
+        });
+      });
     } catch(e){
       if(this.printErrors){
         console.log(sql,4);
@@ -125,16 +106,12 @@ class DataBase {
     }
   }
 
-  canExecute(){
-    return this.enabled === true && this.connection !== null;
-  }
-
   static async executeQuerys(sql,object){
     let result = null,
         length = DataBase.databases.length;
     for(let d = 0; d<length; d++){
       let db = DataBase.databases[d];
-      if(db !== null && db.canExecute() === true){
+      if(db !== null && db.enabled === true && db.connection !== null){
         (result === null) ? result = await db.query(sql,object) : db.query(sql,object);
       }
     }
@@ -142,6 +119,7 @@ class DataBase {
   }
 }
 
+DataBase.enabled=true;
 DataBase.databases=[];
 
 exports.dbQuery = async function(sql,object){
@@ -161,7 +139,6 @@ exports.dbChangeEnable = async function (bool="true"){
       console.log("Finished reading databases, starting loading them");
       DataBase.loadDataBases(databaseConfig);
       console.log("Finished loading databases");
-      DataBase.getStructure();
     } else {
       console.error("Couldn't find database config file");
     }
